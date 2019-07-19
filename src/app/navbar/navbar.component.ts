ceb4f10af7d06fd41c5c8f0f8549ea17a8970d19
iamrames <addictedtomig@gmail.com>
2019-07-19 15:11:05 +0545

    modified navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,7 +8,7 @@ import { AuthService } from '../services/auth.service';
 })
 export class NavbarComponent {
 
-  constructor(private auth: AuthService ) {}
+  constructor(public auth: AuthService ) {}
 
   logout() {
     this.auth.logout();
